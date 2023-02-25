9200076007cef0101d62fd5a6528c3f1ead75169
Aryan-Deshpande <deshpandearyan1@gmail.com>
2023-02-25 14:13:48 +0530

    src

---
+++ src/pytorch/autoencoder.py
@@ -0,0 +1,15 @@
+#torch architecture imports
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision as tv
+from torch.utils.data import Dataset, DataLoader
+import torchvision.transforms as t
+from torchvision import transforms, datasets
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+
+
