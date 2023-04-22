325e633d79fd2956d146363197213700a77b68b0
Aryan Deshpande <deshpandearyan1@gmail.com>
2023-04-22 17:16:16 +0530

    Updated Class, needs attention blocks

--- src/microservice/pytorch/UNet.py
+++ src/microservice/pytorch/UNet.py
@@ -1,55 +1,45 @@
-
-
 import torch 
 import torch.nn as nn
 
 from torchvision import transforms
 
-# backward
-import torch.nn as nn
+from Positional_Embeddings import SinosudialPositionalEncoder
 
-class Attention(nn.Module):
-    def __init__(self,x,t):
-        pass
-
-        self.qkv = nn.Linear(in_ch, 3*in_ch)
-
-    def forward(self,x):
-        pass
-
-class SPE(nn.Module):
-  def __init__(self):
-    super(SPE, self).__init__()
-
-  def forward(self):
-    pass
-
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
 class Block(nn.Module):
   def __init__(self, in_ch, out_ch, time_emb_dim, up=False):
     super(Block, self).__init__()
     
-    self.time_mlp = nn.Linear(time_emb_dim, out_ch) # map the time embedding 
+    self.time_mlp = nn.Linear(time_emb_dim, out_ch, device=device) # map the time embedding 
                                                     # to the out embed
 
-    if up == True:
-      self.conv1 = nn.Conv2d(in_ch, out_ch, 3, padding=1)
-      self.transform = nn.Conv2d(out_ch, out_ch, 4, 2, 1)
-    else:
-      self.conv1 = nn.Conv2d(2*out_ch, in_ch, 3, padding=1)
-      self.transform = nn.ConvTranspose2d(out_ch, out_ch, 4, 2, 1)
+    #self.example = nn.Conv2d(64, 128, 3, padding=1, device=device)
 
-    self.conv2 = nn.Conv2d(out_ch, out_ch, 3, padding=1)
+    if up == True:
+      self.conv1 = nn.Conv2d(in_ch, out_ch, 3, padding=1, device=device)
+      self.transform = nn.Conv2d(out_ch, out_ch, 4, 2, 1, device=device)
+    else:
+      self.conv1 = nn.Conv2d(in_ch, out_ch, 3, padding=1, device=device)
+      self.transform = nn.ConvTranspose2d(out_ch, out_ch, 4, 2, 1, device=device)
+
+    self.conv2 = nn.Conv2d(out_ch, out_ch, 3, padding=1, device=device)
     self.pool = nn.MaxPool2d(3, stride=2)
-    self.bnorm = nn.BatchNorm2d(out_ch)
+    self.bnorm = nn.BatchNorm2d(out_ch, device=device)
     self.relu = nn.ReLU()
   
-  def forward(self, x, t, ):
+  def forward(self, x, t):
     # first conv layer
+    print(('x shape', x.shape))
+    #print('block first conv layer')
+    print('convolution layer weights, biases', self.conv1.weight.shape, self.conv1.bias.shape)
+
     h = self.bnorm(self.relu(self.conv1(x) )) 
+    print('h shape', h.shape)
 
     # time embedding
     time_emb = self.relu(self.time_mlp(t))
+    print('time_emb')
 
     # extend last 2 dim ?
     # here what happens is [(..., )] specifies to retain the existing dimensions
@@ -65,63 +55,87 @@ class Block(nn.Module):
     # down or upsample based on the arguments in the constructor of the class
     return self.transform(h)
 
-class unet(nn.Module):
+class UNetAE(nn.Module):
   def __init__(self):
-    super(unet,self).__init__()
+    super(UNetAE, self).__init__()
+
     image_channels = 3
-    down_channels = (64,128,256,512,1024)
+    down_channels = (64, 128, 256, 512, 1024)
     up_channels = (1024, 512, 256, 128, 64)
 
     out_dim = 1
     time_emb_dim = 32
 
     # Time embedding
-    """self.time_mlp = nn.Sequential(
-        SinusodialPositionEmbeddings(time_emb_dim),
-        nn.Linear(time_emb_dim, time_emb_dim),
-        nn.ReLU()
-    )"""
+    self.time_mlp = nn.Sequential(
+        SinosudialPositionalEncoder(time_emb_dim),
+    )
 
-    self.conv0 = nn.Conv2d(image_channels, down_channels[0], 3, padding=1)
+    self.time_mlp_linear = nn.Sequential(
+      nn.Linear(time_emb_dim, time_emb_dim),
+      nn.ReLU()
+    )
 
-    self.downs = nn.ModuleList( [ Block( down_channels[i], down_channels[i+1], time_emb_dim ) for i in range(len(down_channels)-1 )] )
-    self.ups = nn.ModuleList( [ Block(up_channels[i], up_channels[i+1], time_emb_dim, up=True) for i in range(len(up_channels)-1 )] )
+    self.conv0 = nn.Conv2d(image_channels, down_channels[0], 3, padding=1, device=device)
+
+    self.downs = nn.ModuleList( [ Block( down_channels[i], down_channels[i+1], time_emb_dim ) for i in range( len(down_channels)-1 ) ] )
+    self.ups = nn.ModuleList( [ Block( up_channels[i], up_channels[i+1], time_emb_dim, up=True) for i in range( len(up_channels)-1 ) ] )
     
     self.output = nn.Conv2d(up_channels[-1], 3, out_dim)
 
-  def forward(self, x):
-    x = self.time_mlp(x)
+  def forward(self, x, t):
+    #print('In the UNet module', x)
 
-    first_conv_out = conv0(x)
-
-    for down in downs:
-        residual = []
-        x = down(x)
-
-        residual.append(x)
+    t = self.time_mlp(t) # utilized to embed the time
+    #x = self.time_mlp_linear(x)
+    #print(t, 'timemlp')
+    #print(t.shape, ' shape of timemlp')
     
+    #print(self.conv0.weight.get_device(), self.conv0.bias.get_device(), 'shapes of parameters')
+    first_conv_out = self.conv0(x)
+    #print('after first conv', first_conv_out.shape)
+
+    x = first_conv_out
+    print(x.shape, ' x.shape')
+
+    residual = []
+
+    print(self.downs)
+    print(self.ups)
+    for down in self.downs:
+
+      x = down(x, t)
+
+      residual.append(x)
+
     # residual connection ?
     # attention block
     # residual connection ?
     
-    for up in ups:
-        residual_x = residual.pop()
+    for up in self.ups:
+      residual_x = residual.pop()
 
-        # add the residual as a additional channel
-        x = torch.cat(residual_x, x, dim=1)
-        x = up(x, t)
+      # add the residual as a additional channel
+        
+      x = torch.cat( (residual_x, x), dim=1)
+      x = up(x, t)
 
-        return self.output(x)
+    print(x.shape, 'out of UNet module')
 
-from PIL import Image
-img = Image.open('./da.png')
+    return self.output(x)
 
-trans = transforms.Compose([
-    transforms.ToTensor()
-])
+"""def loss(model, x_0, t):
+  x_noisy, noise = forward_diff()
+  noise_pred = unet(x)
 
-img = trans(img)
-img.shape
+  clip_loss = compute_clip_loss()
 
-model = unet()
+  l1_loss = torch.nn.functional.l1_loss(noise_pred, noise)
+
+  final_loss = l1_loss + clip_loss
+
+  return final_loss"""
+
+
+
 
